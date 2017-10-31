c4eeb983b54f96ee3ac73d8180c6acbd86f4a7eb
Soenke Liebau <soenke.liebau@opencore.com>
2017-10-31 07:33:12 +0100

    Added outbound traffic to security role.

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -43,6 +43,14 @@ resource "aws_security_group_rule" "allow-ssh" {
   security_group_id = "${aws_security_group.oc_kafka.id}"
 }
 
+resource "aws_security_group_rule" "allow-ssh" {
+  type              = "egress"
+  from_port       = 0
+  to_port         = 0
+  protocol        = "-1"
+  cidr_blocks     = ["0.0.0.0/0"]  security_group_id = "${aws_security_group.oc_kafka.id}"
+}
+
 data "aws_ami" "node-ami" {
   most_recent = true
   filter {
